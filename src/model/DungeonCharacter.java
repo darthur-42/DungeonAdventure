84b31f1649d117f652f51c5beb3d08182415d9bf
aujustin14 <tuandungjustin@gmail.com>
2025-02-11 18:13:46 -0800

    Added some documentation to DungeonCharacter.java
    Updated visibility of all methods and the Random variable for better
    encapsulation.
    Fixed bug with calculating hit requirement during attacks.
    Corrected myAttackSpeed type.

--- src/model/DungeonCharacter.java
+++ src/model/DungeonCharacter.java
@@ -1,67 +1,118 @@
 /**
- * 
+ * TCSS 360 Group Project
  */
-package dungeonAdventure;
+package model;
 
 import java.util.Random;
 
 /**
+ * This abstract class represents a generic entity within the Dungeon, with common behaviors that
+ * subclasses must implement. 
  * 
+ * @author Justin Le
+ * @version 11 Feb 2025
  */
 public abstract class DungeonCharacter {
-	private Random random = new Random();
 	
-	private String name;
-	private int healthPoints;
-	private int damageMin;
-	private int damageMax;
-	private double attackSpeed;
-	private double hitChance;
+	protected Random random = new Random();
 	
-	public DungeonCharacter(String theName, int theHealthPoints, int theDamageMin, int theDamageMax, double theAttackSpeed, double theHitChance) {
-		name = theName;
-		healthPoints = theHealthPoints;
-		damageMin = theDamageMin;
-		damageMax = theDamageMax;
-		attackSpeed = theAttackSpeed;
-		hitChance = theHitChance;
+	private String myName;
+	private int myHealthPoints;
+	private int myDamageMin;
+	private int myDamageMax;
+	private int myAttackSpeed;
+	private double myHitChance;
+	
+	protected DungeonCharacter(String theName, int theHealthPoints, int theDamageMin,
+			int theDamageMax, int theAttackSpeed, double theHitChance) {
+		myName = theName;
+		myHealthPoints = theHealthPoints;
+		myDamageMin = theDamageMin;
+		myDamageMax = theDamageMax;
+		myAttackSpeed = theAttackSpeed;
+		myHitChance = theHitChance;
 	}
 	
-	public String getName() {
-		return name;
+	/**
+	 * Returns the character's name.
+	 * 
+	 * @return the character's name
+	 */
+	protected String getName() {
+		return myName;
 	}
 	
-	public int getHealthPoints() {
-		return healthPoints;
+	/**
+	 * Returns the character's health points.
+	 * 
+	 * @return the character's health points
+	 */
+	protected int getHealthPoints() {
+		return myHealthPoints;
 	}
 	
-	public void setHealthPoints(int newHealthPoints) {
-		healthPoints = newHealthPoints;
+	/**
+	 * Sets the character's health points to a new value.
+	 * 
+	 * @param newHealthPoints New health value
+	 */
+	protected void setHealthPoints(int newHealthPoints) {
+		myHealthPoints = newHealthPoints;
 	}
 	
-	public int getDamageMin() {
-		return damageMin;
+	/**
+	 * Returns the character's minimum damage.
+	 * 
+	 * @return the character's minimum damage
+	 */
+	protected int getDamageMin() {
+		return myDamageMin;
 	}
 	
-	public int getDamageMax() {
-		return damageMax;
+	/**
+	 * Returns the character's maximum damage.
+	 * 
+	 * @return the character's maximum damage
+	 */
+	protected int getDamageMax() {
+		return myDamageMax;
 	}
 	
-	public int getRandomDamage() {
+	/**
+	 * Returns a random number in the character's damage range.
+	 * 
+	 * @return a random number in the character's damage range
+	 */
+	protected int getRandomDamage() {
 		return random.nextInt(getDamageMin(), getDamageMax() + 1);
 	}
 	
-	public double getAttackSpeed() {
-		return attackSpeed;
+	/**
+	 * Returns the character's attack speed.
+	 * 
+	 * @return Returns the character's attack speed
+	 */
+	protected int getAttackSpeed() {
+		return myAttackSpeed;
 	}
 	
-	public double getHitChance() {
-		return hitChance;
+	/**
+	 * Returns the character's hit chance.
+	 * 
+	 * @return the character's hit chance
+	 */
+	protected double getHitChance() {
+		return myHitChance;
 	}
 	
-	public void attack(DungeonCharacter otherCharacter) {
+	/**
+	 * Perform an attack on another DungeonCharacter.
+	 * 
+	 * @param otherCharacter the other DungeonCharacter
+	 */
+	protected void attack(DungeonCharacter otherCharacter) {
 		double thisHitChance = random.nextDouble(0, getHitChance());
-		double thisHitRequirement = random.nextDouble(0, 100);
+		double thisHitRequirement = random.nextDouble(0, 1);
 		
 		if (thisHitChance >= thisHitRequirement) {
 			int otherNewHealthPoints = otherCharacter.getHealthPoints() - getRandomDamage();
@@ -69,4 +120,5 @@ public abstract class DungeonCharacter {
 			otherCharacter.setHealthPoints(otherNewHealthPoints);
 		}
 	}
+	
 }
