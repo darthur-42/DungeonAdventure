22048360af2799e4ef346db953b306fe876df38d
unknown <darthur042@gmail.com>
2025-02-18 19:23:24 -0800

    Implimented most of Dungeon and all of Room.

---
+++ src/model/Dungeon.java
@@ -0,0 +1,172 @@
+/**
+ * TCSS 360 Group Project
+ */
+package model;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+/**
+ * This class represents the Dungeon in which the adventure occurs.
+ * It will be implemented as a 2D array of Rooms.
+ * 
+ * @author Arthur Fornia
+ * @version 18 Feb 2025
+ */
+public class Dungeon {
+	
+	/** An integer used in creation of the Map. It sets the max bounds of each array. */
+	private final int MAP_SIZE = 10;
+	
+	/** An integer used in creation of the Dungeon. It sets the max number of reachable rooms. */
+	private final int DUNGEON_SIZE = 20;
+	
+	/** An integer used in creation of the Dungeon. It is the number of Pillars of OOP. */
+	private final int PILLARS = 4;
+	
+	/** 
+	 * An integer used in creation of the Dungeon. It sets the chance potions and pits spawn. 
+	 * Odds are calculated by the chance that a random integer is 1 from 1 to LOOT_CHANCE. 
+	 */
+	private final int LOOT_CHANCE = 10; // 1 - 10 is a 10% chance
+	
+	/** Random object used to generate random numbers. */
+	private Random random = new Random();
+	
+	/** An array of Rooms which are reachable Rooms, used for Random generation. */
+	private ArrayList<Room> activeRooms;
+	
+	/** A 2D array of Rooms which is the Map for the Dungeon. */
+	Room[][] map;
+	
+	/**
+	 * Constructs a Dungeon which is essentially a 2D array of Rooms.
+	 */
+	Dungeon() {
+		map = new Room[MAP_SIZE][MAP_SIZE];
+		activeRooms = new ArrayList<Room>();
+		generateMaze();
+	}
+	
+	/**
+	 * Places an entrance in one of the Rooms of the Dungeon. 
+	 * Ensures there is nothing else in it.
+	 */
+	private void placeEntrance() {
+		Room room = randomActiveRoom();
+		room = new Room(room.hasNorth, room.hasEast, room.hasSouth, room.hasWest);
+		room.hasEntrance = true;
+	}
+	
+	/**
+	 * Places an entrance in one of the Rooms of the Dungeon. 
+	 * Ensures there is nothing else in it.
+	 */
+	private void placeExit() {
+		Room room = randomActiveRoom();
+		while (room.hasEntrance) {
+			room = randomActiveRoom();
+		}
+		
+		room = new Room(room.hasNorth, room.hasEast, room.hasSouth, room.hasWest);
+		room.hasExit = true;
+	}
+	
+	/**
+	 * Places one of each of the four pillars of OOP in four different Rooms of the Dungeon.
+	 * 
+	 * @param theAccumulator is an integer used to track which Pillar will be next. 
+	 */
+	private void placePillars(int theAccumulator) {
+		final char[] pillarIDs = {'A', 'E', 'I', 'P'};
+		Room room = randomActiveRoom();
+		while (room.hasEntrance || room.hasExit || room.hasPillarOO) {
+			room = randomActiveRoom();
+		}
+		
+		room.hasPillarOO = true;
+		room.pillarID = pillarIDs[theAccumulator];
+	}
+	
+	
+	/**
+	 * Goes through activeRooms and Rooms with nothing in them can randomly place objects.
+	 * There is a 10% chance that each of the 3 objects are placed. 
+	 */
+	private void placeOthers() {
+		for (int i = 0; i < activeRooms.size(); i++) {
+			Room currentRoom = activeRooms.get(i);
+			if (!currentRoom.hasEntrance || !currentRoom.hasExit || !currentRoom.hasPillarOO) { 
+				if (random.nextInt(1, LOOT_CHANCE) == 1) {
+					currentRoom.hasHealingPotion = true;
+				}
+				if (random.nextInt(1, LOOT_CHANCE) == 1) {
+					currentRoom.hasVisionPotion = true;
+				}
+				if (random.nextInt(1, LOOT_CHANCE) == 1) {
+					currentRoom.hasPit = true;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Makes all of the doors that connect the Rooms to each other creating a maze like pattern.
+	 * Then calls place methods to put objects into rooms that can be reached. 
+	 */
+	void generateMaze() {
+		Room nextRoom = randomRoom();
+		while (activeRooms.size() < DUNGEON_SIZE) {
+			if (!activeRooms.contains(nextRoom)) {
+				activeRooms.add(nextRoom);
+			}
+			nextRoom = randomRoom();
+		}
+		//TODO Create a maze (randomly generate) by placing the doors, 
+		//currently just picks random disconnected Rooms 
+		placeEntrance();
+		placeExit();
+		for (int i = 0; i < PILLARS; i++) { 
+			placePillars(i);
+		}
+		placeOthers();
+	}
+	
+	/**
+	 * Picks a room at random from map.
+	 * 
+	 * @return a random Room from map.
+	 */
+	private Room randomRoom() { 
+		return map[random.nextInt(0, MAP_SIZE - 1)][random.nextInt(0, MAP_SIZE - 1)];
+	}
+	
+	/**
+	 * Picks a room at random from activeRooms.
+	 * 
+	 * @return a random Room from activeRooms.
+	 */
+	private Room randomActiveRoom() { 
+		return activeRooms.get(random.nextInt(0, DUNGEON_SIZE - 1)); 
+	}
+	
+	/**
+	 * Creates a String that represents the Dungeon to help visualize it. 
+	 * 
+	 * @return a String that represents the Dungeon.
+	 */
+	@Override
+	public String toString() {
+		String output = "Map:\n";
+		for (int y = 0; y < MAP_SIZE; y++) {
+			for (int x = 0; x < MAP_SIZE; x++) {
+				output += map[y][x].toString();
+			}
+			output += "\n"; //new line after each row
+		}
+		
+		output += "\n";
+		output += "List of reachable Rooms: " + activeRooms.toString() + "\n";
+		return output;
+	}
+}
---
+++ src/model/Room.java
@@ -0,0 +1,187 @@
+/**
+ * TCSS 360 Group Project
+ */
+package model;
+
+/**
+ * This class represents the Rooms of the Dungeon in which the adventure occurs.
+ * 
+ * @author Arthur Fornia
+ * @version 18 Feb 2025
+ */
+class Room {
+	
+	/** If the Room has the Entrance. */
+	boolean hasEntrance;
+	
+	/** If the Room has the Exit. */
+	boolean hasExit;
+	
+	/** If the Room has a Healing Potion in it. */
+	boolean hasHealingPotion;
+	
+	/** If the Room has a Vision Potion in it. */
+	boolean hasVisionPotion;
+	
+	/** If the Room has a Pit in it. */
+	boolean hasPit;
+	
+	/** If the Room has a Pillar of OOP in it. */
+	boolean hasPillarOO;
+	
+	/** If the Room has a Door to the North. */
+	boolean hasNorth;
+	
+	/** If the Room has a Door to the East. */
+	boolean hasEast;
+	
+	/** If the Room has a Door to the South. */
+	boolean hasSouth;
+	
+	/** If the Room has a Door to the West. */
+	boolean hasWest;
+	
+	/** An ID Character to Identify the Pillar of OOP in the Room, '?' if none. */
+	char pillarID;
+	
+	/**
+	 * Constructs a Room which is an element of the 2D array of Dungeon. 
+	 * This is an empty Room with no connecting Rooms.
+	 */
+	Room() {
+		this.hasEntrance = false;
+		this.hasExit = false;
+		this.hasHealingPotion = false;
+		this.hasVisionPotion = false;
+		this.hasPit = false;
+		this.hasPillarOO = false;
+		this.hasNorth = false;
+		this.hasEast = false;
+		this.hasSouth = false;
+		this.hasWest = false;
+		this.pillarID = '?';
+	}
+	
+	/**
+	 * Constructs a Room which is an element of the 2D array of Dungeon. 
+	 * This is an empty Room with inputed connecting Rooms.
+	 * 
+	 * @param theNorth if there is a Room north of this Room
+	 * @param theEast if there is a Room east of this Room
+	 * @param theSouth if there is a Room south of this Room
+	 * @param theWest if there is a Room west of this Room
+	 */
+	Room(final boolean theNorth, final boolean theEast, final boolean theSouth, 
+			final boolean theWest) {
+		this();
+		this.hasNorth = theNorth;
+		this.hasEast = theEast;
+		this.hasSouth = theSouth;
+		this.hasWest = theWest;
+	}
+	
+	/**
+	 * Creates and returns a String that represents the Room to help with testing. 
+	 * 
+	 * @return a String that displays all of the data stored in the Room. 
+	 */
+	String testString() {
+		String PillarName;
+		if (this.pillarID == 'A') {
+			PillarName = "Abstraction";
+		} else if (this.pillarID == 'E') {
+			PillarName = "Encapsulation";
+		} else if (this.pillarID == 'I') {
+			PillarName = "Inheritance";
+		} else if (this.pillarID == 'P') {
+			PillarName = "Polymorphism";
+		} else {
+			PillarName = "No Pillar";
+		}
+		
+		return "Room: [" + "Entrance: " + this.hasEntrance + "Exit: " + this.hasExit + 
+				"Healing Potion: " + this.hasHealingPotion + "Vision Potion: " + 
+				this.hasVisionPotion + "Pit: " + this.hasPit + "OOPillar: " + this.hasPillarOO + 
+				"Pillar Name: " + PillarName + "Doors: {" + "North: " + this.hasNorth + "East: " +
+				this.hasEast + "South: " + this.hasSouth + "West: " + this.hasWest + "}]";
+	}
+	
+	/**
+	 * Creates and returns a String that represents the Room to help visualize it. 
+	 * Walls and corners = *
+	 * Doors = - or | depending on orientation
+	 * Multiple Items = M
+	 * Pit = X
+	 * Entrance = i (in)
+	 * Exit = O (Out)
+	 * Healing Potion = H
+	 * Vision Potion = V
+	 * Empty Room = " " (space)
+	 * Pillars = A, E, I, or P
+	 * 
+	 * 
+	 * @return a String that displays all of the data stored in the Room in command line UI. 
+	 */
+	@Override
+	public String toString() {
+		String output = "*"; //north west corner
+		
+		if (this.hasNorth) { //north center
+			output += "-";
+		} else {
+			output += "*";
+		} //end north center
+		
+		output += "*\n"; //north east corner
+		
+		if (this.hasWest) { //west center
+			output += "|";
+		} else {
+			output += "*";
+		} //end west center
+		
+		char center = '?'; //center
+		int accumulator = 0;
+		if (this.hasEntrance) {
+			center = 'i';
+			accumulator++;
+		} else if (this.hasExit) {
+			center = 'O';
+			accumulator++;
+		} else if (this.hasHealingPotion) {
+			center = 'H';
+			accumulator++;
+		} else if (this.hasVisionPotion) {
+			center = 'V';
+			accumulator++;
+		} else if (this.hasPit) {
+			center = 'X';
+			accumulator++;
+		} else if (this.hasPillarOO) {
+			center = pillarID;
+			accumulator++;
+		} else if (accumulator > 1 && accumulator != 0) {
+			center = 'M';
+		} else {
+			center = ' ';
+		}
+		output += center; //end center
+		
+		if (this.hasEast) { //east center
+			output += "|";
+		} else {
+			output += "*\n";
+		} //end east center
+		
+		output += "*"; //south west corner
+		
+		if (this.hasNorth) { //south center
+			output += "-";
+		} else {
+			output += "*";
+		} //end south center
+		
+		output += "*"; //south east corner
+		return output;
+	}
+}
