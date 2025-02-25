bd5ef6e165c64906163bed75da3524b4bb157ef8
Anna Brewer <annaa.brewer@gmail.com>
2025-02-25 01:56:34 -0800

    Added database stats constructor to Monster subclasses (Gremlin, Ogre, Skeleton)

--- src/model/Ogre.java
+++ src/model/Ogre.java
@@ -12,30 +12,49 @@ import java.util.Random;
  * @version 22 Feb 2025
  */
 public class Ogre extends Monster {
-    
-    /**
-     * Constructs an Ogre.
-     */
-	protected Ogre() {
-        this(new Random());
-    }
-    
-    /**
-     * Constructs an Ogre.
-     */
-	protected Ogre(final Random theRandom) {
-        super("Ogre", 200, 30, 60, 2, 0.6, 30, 60, 0.1, theRandom);
-    }
 
 	/**
-     * Attacks the target and may heal.
-     * 
-     * @param theTarget the character being attacked
-     */
-    @Override
-    protected void castAttackOn(final DungeonCharacter theTarget) {
-        System.out.println(getName() + " attacks " + theTarget.getName() + "!");
-        attack(theTarget);
-        heal();
-    }
+	 * Constructs an Ogre with stats from the database.
+	 * 
+	 * @param theHealthPoints the monster's health points
+	 * @param theDamageMin    the minimum damage
+	 * @param theDamageMax    the maximum damage
+	 * @param theAttackSpeed  the attack speed
+	 * @param theHitChance    the chance to hit
+	 * @param theHealMin      the minimum heal amount
+	 * @param theHealMax      the maximum heal amount
+	 * @param theHealChance   the chance to heal
+	 * @param theRandom       the Random instance
+	 */
+	public Ogre(int theHealthPoints, int theDamageMin, int theDamageMax, int theAttackSpeed, double theHitChance,
+			int theHealMin, int theHealMax, double theHealChance, Random theRandom) {
+		super("Ogre", theHealthPoints, theDamageMin, theDamageMax, theAttackSpeed, theHitChance, theHealMin, theHealMax,
+				theHealChance, theRandom);
+	}
+
+	/**
+	 * Constructs an Ogre.
+	 */
+	public Ogre() {
+		this(new Random());
+	}
+
+	/**
+	 * Constructs an Ogre. Can pass in a random instance for testing.
+	 */
+	public Ogre(final Random theRandom) {
+		super("Ogre", 200, 30, 60, 2, 0.6, 30, 60, 0.1, theRandom);
+	}
+
+	/**
+	 * Attacks the target character and may heal.
+	 * 
+	 * @param otherCharacter the character being attacked
+	 */
+	@Override
+	public void castAttackOn(final DungeonCharacter otherCharacter) {
+		System.out.println(getName() + " attacks " + otherCharacter.getName() + "!");
+		attack(otherCharacter);
+		heal();
+	}
 }
--- src/model/Skeleton.java
+++ src/model/Skeleton.java
@@ -12,30 +12,49 @@ import java.util.Random;
  * @version 22 Feb 2025
  */
 public class Skeleton extends Monster {
-    
-    /**
-     * Constructs a Skeleton..
-     */
-	protected Skeleton() {
-        this(new Random());
-    }
-    
-    /**
-     * Constructs a Skeleton..
-     */
-	protected Skeleton(final Random theRandom) {
-        super("Skeleton", 100, 30, 50, 3, 0.8, 30, 50, 0.3, theRandom);
-    }
 
 	/**
-     * Attacks the target and may heal.
-     * 
-     * @param theTarget the character being attacked
-     */
-    @Override
-    protected void castAttackOn(final DungeonCharacter theTarget) {
-        System.out.println(getName() + " attacks " + theTarget.getName() + "!");
-        attack(theTarget);
-        heal();
-    }
+	 * Constructs a Gremlin with stats from the database.
+	 * 
+	 * @param theHealthPoints the monster's health points
+	 * @param theDamageMin    the minimum damage
+	 * @param theDamageMax    the maximum damage
+	 * @param theAttackSpeed  the attack speed
+	 * @param theHitChance    the chance to hit
+	 * @param theHealMin      the minimum heal amount
+	 * @param theHealMax      the maximum heal amount
+	 * @param theHealChance   the chance to heal
+	 * @param theRandom       the Random instance
+	 */
+	public Skeleton(int theHealthPoints, int theDamageMin, int theDamageMax, int theAttackSpeed, double theHitChance,
+			int theHealMin, int theHealMax, double theHealChance, Random theRandom) {
+		super("Skeleton", theHealthPoints, theDamageMin, theDamageMax, theAttackSpeed, theHitChance, theHealMin,
+				theHealMax, theHealChance, theRandom);
+	}
+
+	/**
+	 * Constructs a Skeleton.
+	 */
+	public Skeleton() {
+		this(new Random());
+	}
+
+	/**
+	 * Constructs a Skeleton. Can pass in a random instance for testing.
+	 */
+	public Skeleton(final Random theRandom) {
+		super("Skeleton", 100, 30, 50, 3, 0.8, 30, 50, 0.3, theRandom);
+	}
+
+	/**
+	 * Attacks the target character and may heal.
+	 * 
+	 * @param otherCharacter the character being attacked
+	 */
+	@Override
+	public void castAttackOn(final DungeonCharacter otherCharacter) {
+		System.out.println(getName() + " attacks " + otherCharacter.getName() + "!");
+		attack(otherCharacter);
+		heal();
+	}
 }
