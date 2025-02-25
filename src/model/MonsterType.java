d28eb89834e649a4f00acc6f071927b51f7dc2ba
Anna Brewer <annaa.brewer@gmail.com>
2025-02-25 02:01:27 -0800

    Created MonsterDatabase, MonsterFactory, MonsterType, added SQLite JDBC library, and monsters.db

---
+++ src/model/MonsterType.java
@@ -0,0 +1,31 @@
+/**
+ * TCSS 360 Group Project
+ */
+
+package model;
+
+/**
+ * Represents the types of monsters that can be created.
+ * 
+ * @author Anna Brewer
+ * @version 23 Feb 2025
+ */
+public enum MonsterType {
+	GREMLIN, OGRE, SKELETON;
+
+	/**
+	 * Returns the MonsterType that matches the given name.
+	 *
+	 * @param theName the monster name
+	 * @return the matching MonsterType
+	 * @throws IllegalArgumentException if the name is invalid
+	 */
+	public static MonsterType fromString(final String theName) {
+		return switch (theName.trim().toUpperCase()) {
+		case "GREMLIN" -> GREMLIN;
+		case "OGRE" -> OGRE;
+		case "SKELETON" -> SKELETON;
+		default -> throw new IllegalArgumentException("Invalid monster name");
+		};
+	}
+}
