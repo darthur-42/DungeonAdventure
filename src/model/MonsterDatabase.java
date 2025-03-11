9f8602e20d5f2aa15f420954138d50069f3a4171
Anna Brewer <annaa.brewer@gmail.com>
2025-03-11 10:20:59 -0700

    Updated MonsterDatabase to separate Factory from Database

--- src/model/MonsterDatabase.java
+++ src/model/MonsterDatabase.java
@@ -9,7 +9,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.sqlite.SQLiteDataSource;
 
@@ -17,14 +16,13 @@ import org.sqlite.SQLiteDataSource;
  * Stores and retrieves Monster data in a SQLite database.
  *
  * @author Anna Brewer
- * @version 3 Mar 2025
+ * @version 11 Mar 2025
  */
 public class MonsterDatabase {
     private static final String DB_FILE = "jdbc:sqlite:monsters.db";
 
     private Connection myConn;
     private Statement myStmt;
-    private final Random myRandom = new Random();
 
     /**
      * Establishes a connection to the SQLite database.
@@ -48,12 +46,12 @@ public class MonsterDatabase {
     }
 
     /**
-     * Retrieves all Monsters from the database.
+     * Retrieves all Monsters' raw data from the database.
      *
-     * @return a list of Monster objects
+     * @return a list of Object arrays containing monster data
      */
-    public List<Monster> getAllMonsters() {
-        List<Monster> monsters = new ArrayList<>();
+    public List<Object[]> getAllMonstersData() {
+        List<Object[]> monsterDataList = new ArrayList<>();
         
         String query = "SELECT * FROM monsters";
 
@@ -62,36 +60,35 @@ public class MonsterDatabase {
              ResultSet rs = stmt.executeQuery(query)) {
             
             while (rs.next()) {
-                monsters.add(generateMonster(rs));
+                monsterDataList.add(retrieveMonsterData(rs));
             }
             
         } catch (SQLException e) {
             System.err.println("Error retrieving monsters from database: " + e.getMessage());
         }
         
-        return monsters;
+        return monsterDataList;
     }
 
     /**
-     * Generates a Monster using data from the database.
+     * Retrieves Monster data from a ResultSet.
      *
      * @param theRs the ResultSet containing monster data
-     * @return the Monster
+     * @return an Object array with the monster's stored data
      */
-    private Monster generateMonster(ResultSet theRs) throws SQLException {
-    	MonsterType type = MonsterType.fromString(theRs.getString("name"));
-    	
-        int healthPoints = theRs.getInt("health_points");
-        int damageMin = theRs.getInt("damage_min");
-        int damageMax = theRs.getInt("damage_max");
-        int attackSpeed = theRs.getInt("attack_speed");
-        double hitChance = theRs.getDouble("hit_chance");
-        int healMin = theRs.getInt("heal_min");
-        int healMax = theRs.getInt("heal_max");
-        double healChance = theRs.getDouble("heal_chance");
-        return MonsterFactory.createMonster(type, healthPoints, damageMin, damageMax, attackSpeed,
-                hitChance, healMin, healMax, healChance, myRandom);
-}
+    private Object[] retrieveMonsterData(ResultSet theRs) throws SQLException {
+        return new Object[]{
+            theRs.getString("name"),
+            theRs.getInt("health_points"),
+            theRs.getInt("damage_min"),
+            theRs.getInt("damage_max"),
+            theRs.getInt("attack_speed"),
+            theRs.getDouble("hit_chance"),
+            theRs.getInt("heal_min"),
+            theRs.getInt("heal_max"),
+            theRs.getDouble("heal_chance")
+        };
+    }
 
     /**
      * Closes the database connection when done.
